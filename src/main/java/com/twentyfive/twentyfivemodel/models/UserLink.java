85954cc744884a1af16a6b609fe5a8d094d4cab6
Twentyfivesrls <twentyfivesrls@gmail.com>
2023-09-14 15:30:30 +0200

    Error constructor fixed

--- src/main/java/com/twentyfive/twentyfivemodel/models/UserLink.java
+++ src/main/java/com/twentyfive/twentyfivemodel/models/UserLink.java
@@ -3,14 +3,13 @@ package com.twentyfive.twentyfivemodel.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@RequiredArgsConstructor
+
 public class UserLink {
     private String id;
     private String userId;
