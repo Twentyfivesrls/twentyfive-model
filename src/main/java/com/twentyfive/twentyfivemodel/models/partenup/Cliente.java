b09d324ecb021661d4beffbbcfb32b0341e484d4
Raikozu <francescogaglianese93@gmail.com>
2023-12-14 11:01:49 +0100

    partenup fixed cliente model

--- src/main/java/com/twentyfive/twentyfivemodel/models/partenup/Cliente.java
+++ src/main/java/com/twentyfive/twentyfivemodel/models/partenup/Cliente.java
@@ -3,9 +3,13 @@ package com.twentyfive.twentyfivemodel.models.partenup;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @Entity
 @Data
@@ -19,4 +23,19 @@ public class Cliente {
     private String nomecliente;
 
     private String partitaiva;
+
+    @OneToMany()
+    private List<PuntoVendita> listapuntivendita = new LinkedList<>();
+
+    private double marginegasolioservito;
+    private double marginegasolioself;
+
+    private double marginebenzinaservito;
+    private double marginebenzinaself;
+
+    private double marginesupremeservito;
+    private double marginesupremeself;
+
+    private double marginegplservito;
+    private double marginegplself;
 }
