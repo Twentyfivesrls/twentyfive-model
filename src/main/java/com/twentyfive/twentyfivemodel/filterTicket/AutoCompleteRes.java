f256c620f495a9b221060461a5d4fc1ef49e6d07
Raikozu <francescogaglianese93@gmail.com>
2024-01-16 17:07:59 +0100

    Aggiunta classe per un Autocomplete

---
+++ src/main/java/com/twentyfive/twentyfivemodel/filterTicket/AutoCompleteRes.java
@@ -0,0 +1,12 @@
+package com.twentyfive.twentyfivemodel.filterTicket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AutoCompleteRes {
+    private String value;
+}
